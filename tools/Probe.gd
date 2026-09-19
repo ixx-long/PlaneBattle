07188fae6a820723c2ed830507b83ea55e2e4b9c
IXX-long <2060724617@qq.com>
2026-09-19 18:32:31 +0800

    移除与游戏无关的临时探针 tools/Probe.gd
    它是早期用来打印引擎版本与键码常量的临时脚本，不属于游戏、也不被任何工具或文档引用。

--- tools/Probe.gd
+++
@@ -1,6 +0,0 @@
-extends SceneTree
-
-func _initialize() -> void:
-	print(Engine.get_version_info())
-	print("KEY_LEFT=", KEY_LEFT, " KEY_UP=", KEY_UP, " KEY_RIGHT=", KEY_RIGHT, " KEY_DOWN=", KEY_DOWN)
-	quit()
